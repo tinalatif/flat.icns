1f4e1428d8cf6369127b0e3d01d897ee92e8d3f9
tinalatif <tina.tl.latif@gmail.com>
2014-05-28 01:52:03 -0400

    icon structure + more responsiveness

--- website/js/main.js
+++ website/js/main.js
@@ -7,4 +7,38 @@ $(document).ready(function() {
 	if($("#info-block").height() > $("#info-container").height()) { 
 		$("#info-container").css("height", $("#info-block").height());
 	}	
+
+	var overflow = $(document.body).height() < $("#icon-container").height();
+
+	if(!overflow) { 
+		$("#icons").css("padding-top", "0px");
+		$("#icons").css("padding-bottom", "0px");
+		$("#icons").addClass("icons-centered");
+
+		$("#icon-container").css("height", "calc(100% - 50px)");
+	}
+
+	$(".see-more-btn").click(function() { 		
+
+		if(overflow) { 
+			$("#hidden-icons").show();
+			$(this).hide();
+		}
+
+		else { 
+			var oldTop = $("#icons").css("top");
+			var oldTransform = $("#icons").css("transform");
+
+			$("#hidden-icons").show();
+			$("#icon-container").css("height", 
+			$("#icon-container").height() + $("#hidden-icons").height() - $((this)).height() - 45);
+		
+			$(this).hide();
+
+			$("#icons").css("top", oldTop);
+			$("#icons").css("transform", oldTransform);		
+		}
+		
+	});
 });
+
